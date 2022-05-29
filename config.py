6106f263a0deadd9d320bbe71a310478da413602
cmulonyiwa <cmulonyiwa@gmail.com>
2022-05-29 17:45:59 +0200

    fix

--- config.py
+++ config.py
@@ -1,10 +1,11 @@
 import os
 
 class Config:
-    SECRET_KEY = os.environ.get('strong_key' , 'SECRET_KEY')
+    SECRET_KEY = os.environ.get('SECRET_KEY', 'strong_key')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 class DevConfig(Config):
-    SQLALCHEMY_DATABASE_URI = os.environ.get('sqlite:///data.db' , 'FLASK_DATA')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('FLASK_DATA', 'sqlite:///data.db' )
 
 class TestingConfig(Config):
     TESTING = True
