6b09a9af9b13274f1fba1c9218e42c240ad2185d
cmulonyiwa <cmulonyiwa@gmail.com>
2022-06-01 08:45:55 +0200

    import flask_migrate and add some auto import

--- run.py
+++ run.py
@@ -1,6 +1,15 @@
-from app import create_app
+from flask_migrate import Migrate
+from app import create_app, db
+from app.models import Role
 
 app = create_app('devconfig')
+
+migrate = Migrate(app,db)
+
+
+@app.shell_context_processor
+def py_auto_import():
+    return dict(db=db , Role=Role)
 
 @app.cli.command()
 def test():
