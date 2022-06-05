6d601525791b2fb4f2c7c1cd9dac639a8ff0e51c
cmulonyiwa <cmulonyiwa@gmail.com>
2022-06-05 08:21:15 +0200

    add ADMIN_EMAIL config variable

--- config.py
+++ config.py
@@ -3,6 +3,7 @@ import os
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY', 'strong_key')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    ADMIN_EMAIL = os.environ.get('ADMIN_EMAIL')
 
 class DevConfig(Config):
     SQLALCHEMY_DATABASE_URI = os.environ.get('FLASK_DATA', 'sqlite:///data.db' )
