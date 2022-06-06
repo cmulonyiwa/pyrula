afd592e79f0c277b8933775dead6570ab12804a1
cmulonyiwa <cmulonyiwa@gmail.com>
2022-06-06 12:43:28 +0200

    import flask_login

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,18 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
+from flask_login import LoginManager
 from config import config
 
 db = SQLAlchemy()
+login_manager = LoginManager()
+login_manager.login_view = 'auth.login'
 
 def create_app(app_config):
     app = Flask(__name__)
     app.config.from_object(config[app_config])
     
     db.init_app(app)
+    login_manager.init_app(app)
 
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
