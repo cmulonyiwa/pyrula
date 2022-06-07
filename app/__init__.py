0d816bf0feb2f9ab6d51b8b7c4ecda485198b80d
cmulonyiwa <cmulonyiwa@gmail.com>
2022-06-07 06:48:02 +0200

    register auth blueprint with application

--- app/__init__.py
+++ app/__init__.py
@@ -15,6 +15,9 @@ def create_app(app_config):
     login_manager.init_app(app)
 
     from .main import main as main_blueprint
+    from .auth import auth as auth_blueprint
+
     app.register_blueprint(main_blueprint)
+    app.register_blueprint(auth_blueprint, url_prefix='/auth')
 
     return app 
